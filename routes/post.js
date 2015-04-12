fa642916cc0920f59fd274c310f7d4cba9de9596
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-12 13:19:48 +0900

    Validate required field

--- routes/post.js
+++ routes/post.js
@@ -1,27 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var validate = require('../lib/middleware/validate');
+
 var Entry = require('../lib/entry');
 
 router.get('/', function(req, res) {
+  req.flash();
   res.render('post', { title: 'Post' });
 });
 
-router.post('/', function(req, res, next) {
-  var data = req.body.entry;
+router.post('/',
+  validate.required('entry[title]'),
+  function(req, res, next) {
+    var data = req.body.entry;
 
-  var entry = new Entry({
-    username: res.locals.user.name,
-    title: data.title,
-    body: data.body
-  });
+    var entry = new Entry({
+      username: res.locals.user.name,
+      title: data.title,
+      body: data.body
+    });
 
-  entry.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect('/');
-  });
-});
+    entry.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  }
+);
 
 module.exports = router;
