c0ba899e3672e04ea5b934eeae21271778b0bde4
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-13 14:16:55 +0900

    Paging

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var page = require('../lib/middleware/page');
+var Entry = require('../lib/entry');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', page(Entry.count, 5), function(req, res, next) {
+  var page = req.page;
+  Entry.getRange(page.from, page.to, function(err, entries) {
+    res.render('entries', {
+      title: 'Entries',
+      entries: entries
+    });
+  });
 });
 
 module.exports = router;
