2e87674bc56816a4199f69b9e2b2ec378298c2ae
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-25 22:16:34 +0900

    Fix

--- routes/settings.js
+++ routes/settings.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../lib/user');
+
 router.get('/', function(req, res) {
   if (!res.locals.user) {
     return res.redirect('/login');
@@ -24,16 +26,47 @@ router.post('/', function(req, res, next) {
     return res.redirect('back');
   }
 
-  user.name = data.name;
-  user.fullname = data.fullname;
+  if (user.name !== data.name) {
+    // If username is changed make sure new username is not already taken.
+    User.getByName(data.name, function(err, existingUser) {
+      if (err) {
+        return next(err);
+      }
+      if (existingUser.id) {
+        req.flash('error', 'Username already taken!');
+        return res.redirect('back');
+      }
 
-  user.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    req.flash('info', 'Thanks, your settings have been saved.');
-    res.redirect('back');
-  });
+      // Delete old ID index
+      User.deleteId(user.name, function(err) {
+        if (err) {
+          return next(err);
+        }
+
+        user.name = data.name
+        user.fullname = data.fullname;
+
+        user.save(function(err) {
+          if (err) {
+            return next(err);
+          }
+          req.flash('info', 'Thanks, your settings have been saved.');
+          res.redirect('back');
+        });
+      });
+    });
+  } else {
+    // Otherwise, update user's full name.
+    user.fullname = data.fullname;
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      req.flash('info', 'Thanks, your settings have been saved.');
+      res.redirect('back');
+    });
+  }
 });
 
 module.exports = router;
