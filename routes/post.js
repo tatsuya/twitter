321714c29c798e9324f2687037279a5634f26d3f
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-26 16:37:10 +0900

    Styles

--- routes/post.js
+++ routes/post.js
@@ -4,8 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+  if (!res.locals.user) {
+    return res.redirect('/login');
+  }
   req.flash();
-  res.render('post', { title: 'Post' });
+  res.render('post', { title: 'Tweet' });
 });
 
 module.exports = router;
