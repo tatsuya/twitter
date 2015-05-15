9dce0569c21be54fd8f2cfd5e3431b03e5843382
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-05-15 22:02:52 +0900

    Doc

--- routes/users.js
+++ routes/users.js
@@ -35,6 +35,71 @@ router.get('/:name', isMe(), function(req, res, next) {
       return next(err);
     }
     async.parallel({
+      followerIds: function(fn) {
+        User.getFollowerIds(user.id, fn);
+      },
+      followingIds: function(fn) {
+        User.getFollowingIds(user.id, fn);
+      },
+      isFollowing: function(fn) {
+        // Check if the user is followed by the user who is currently logged in.
+        if (!me) {
+          return fn(null, false);
+        }
+        User.isFollowing(user.id, me.id, fn);
+      }
+    }, function(err, results) {
+      if (err) {
+        return next(err);
+      }
+
+      var followerIds = results.followerIds;
+      var followingIds = results.followingIds;
+      var isFollowing = results.isFollowing;
+
+      Tweet.filter(function filterByUsername(tweet) {
+        if (!tweet.user) {
+          return false;
+        }
+        return tweet.user.id === user.id;
+      }, function(err, tweets) {
+        if (err) {
+          return next(err);
+        }
+
+        var formattedTweets = tweets.map(function timeCreatedAtFromNow(tweet) {
+          // Pass true to get the value without the suffix.
+          //
+          // Examples:
+          //   moment([2007, 0, 29]).fromNow();     // 4 years ago
+          //   moment([2007, 0, 29]).fromNow(true); // 4 years
+          tweet.created_at = moment(tweet.created_at).fromNow(true);
+          return tweet;
+        });
+
+        res.render('users', {
+          title: util.format('%s (@%s)', user.fullname, user.name),
+          user: user,
+          tweets: formattedTweets,
+          tweets_count: tweets.length,
+          followers_count: followerIds.length,
+          followings_count: followingIds.length,
+          is_following: isFollowing
+        });
+      });
+    });
+  });
+});
+
+router.get('/:name/followers', function(req, res, next) {
+  var me = res.locals.user;
+  var name = req.params.name;
+
+  User.getByName(name, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    async.parallel({
       followers: function(fn) {
         User.getFollowers(user.id, fn);
       },
@@ -77,82 +142,15 @@ router.get('/:name', isMe(), function(req, res, next) {
           return tweet;
         });
 
-        res.render('users', {
-          title: util.format('%s (@%s)', user.fullname, user.name),
-          user: user,
-          followers: followers,
-          followers_count: followers.length,
-          followings_count: followingIds.length,
-          is_following: isFollowing,
-          tweets: formattedTweets,
-          tweets_count: tweets.length
-        });
-      });
-    });
-  });
-});
-
-router.get('/:name/followers', function(req, res, next) {
-  var me = res.locals.user;
-  var name = req.params.name;
-
-  User.getByName(name, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    async.parallel({
-      followers: function(fn) {
-        User.getFollowers(user.id, fn);
-      },
-      followings: function(fn) {
-        User.getFollowings(user.id, fn);
-      },
-      isFollowing: function(fn) {
-        // Check if the user is followed by the user who is currently logged in.
-        if (!me) {
-          return fn(null, false);
-        }
-        User.isFollowing(user.id, me.id, fn);
-      }
-    }, function(err, results) {
-      if (err) {
-        return next(err);
-      }
-
-      var followers = results.followers;
-      var followings = results.followings;
-      var isFollowing = results.isFollowing;
-
-      Tweet.filter(function filterByUsername(tweet) {
-        if (!tweet.user) {
-          return false;
-        }
-        return tweet.user.id === user.id;
-      }, function(err, tweets) {
-        if (err) {
-          return next(err);
-        }
-
-        var formattedTweets = tweets.map(function timeCreatedAtFromNow(tweet) {
-          // Pass true to get the value without the suffix.
-          //
-          // Examples:
-          //   moment([2007, 0, 29]).fromNow();     // 4 years ago
-          //   moment([2007, 0, 29]).fromNow(true); // 4 years
-          tweet.created_at = moment(tweet.created_at).fromNow(true);
-          return tweet;
-        });
-
         res.render('followers', {
           title: util.format('People following %s', user.fullname),
           user: user,
+          tweets: formattedTweets,
+          tweets_count: tweets.length,
           followers: followers,
           followers_count: followers.length,
-          followings: followings,
-          followings_count: followings.length,
-          is_following: isFollowing,
-          tweets: formattedTweets,
-          tweets_count: tweets.length
+          followings_count: followingIds.length,
+          is_following: isFollowing
         });
       });
     });
@@ -168,8 +166,8 @@ router.get('/:name/followings', function(req, res, next) {
       return next(err);
     }
     async.parallel({
-      followers: function(fn) {
-        User.getFollowers(user.id, fn);
+      followerIds: function(fn) {
+        User.getFollowerIds(user.id, fn);
       },
       followings: function(fn) {
         User.getFollowings(user.id, fn);
@@ -186,7 +184,7 @@ router.get('/:name/followings', function(req, res, next) {
         return next(err);
       }
 
-      var followers = results.followers;
+      var followerIds = results.followerIds;
       var followings = results.followings;
       var isFollowing = results.isFollowing;
 
@@ -213,8 +211,7 @@ router.get('/:name/followings', function(req, res, next) {
         res.render('followings', {
           title: util.format('People followed by %s', user.fullname),
           user: user,
-          followers: followers,
-          followers_count: followers.length,
+          followers_count: followerIds.length,
           followings: followings,
           followings_count: followings.length,
           is_following: isFollowing,
