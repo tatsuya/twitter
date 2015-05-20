594f07d1e6f4f4a67d8752dde2cba5025a867718
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-05-20 09:55:01 +0900

    Validation

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 
+var validate = require('../lib/middleware/validate');
 var User = require('../lib/model/user');
 
 router.get('/', function(req, res) {
@@ -10,34 +11,41 @@ router.get('/', function(req, res) {
   res.render('register', { title: 'Register' });
 });
 
-router.post('/', function(req, res, next) {
-  var data = req.body.user;
-  User.getByName(data.name, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-
-    if (user.id) {
-      req.flash('error', 'Username already taken!');
-      // A back redirection redirects the request back to the referer,
-      // defaulting to / when the referer is missing.
-      return res.redirect('back');
-    }
-
-    user = new User({
-      name: data.name,
-      pass: data.pass,
-      fullname: data.fullname
-    });
-
-    user.save(function(err) {
+router.post('/',
+  validate.required('user[name]'),
+  validate.lengthLessThanOrEqualTo('user[name]', 15),
+  validate.required('user[pass]'),
+  validate.required('user[fullname]'),
+  validate.lengthLessThanOrEqualTo('user[fullname]', 20),
+  function(req, res, next) {
+    var data = req.body.user;
+    User.getByName(data.name, function(err, user) {
       if (err) {
         return next(err);
       }
-      req.session.uid = user.id;
-      res.redirect('/');
+
+      if (user.id) {
+        req.flash('error', 'Username already taken!');
+        // A back redirection redirects the request back to the referer,
+        // defaulting to / when the referer is missing.
+        return res.redirect('back');
+      }
+
+      user = new User({
+        name: data.name,
+        pass: data.pass,
+        fullname: data.fullname
+      });
+
+      user.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        req.session.uid = user.id;
+        res.redirect('/');
+      });
     });
-  });
-});
+  }
+);
 
 module.exports = router;
